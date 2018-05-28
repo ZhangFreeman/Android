339dbf93f0bcd62de509c9ae11e0d78e74656659
ZhangFreeman <zhang.xs.titech@gmail.com>
2018-05-29 07:34:38 +0900

    little update

--- app/src/main/java/com/android/my/zhang/dribbview/view/bucket_list/BucketListFragment.java
+++ app/src/main/java/com/android/my/zhang/dribbview/view/bucket_list/BucketListFragment.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.os.AsyncTaskCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -22,9 +21,9 @@ import android.view.ViewGroup;
 
 import com.android.my.zhang.dribbview.R;
 import com.android.my.zhang.dribbview.dribbble.auth.Dribbble;
-import com.google.gson.JsonSyntaxException;
 import com.android.my.zhang.dribbview.model.Bucket;
 import com.android.my.zhang.dribbview.view.base.SpaceItemDecoration;
+import com.google.gson.JsonSyntaxException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -111,7 +110,7 @@ public class BucketListFragment extends Fragment {
             String bucketName = data.getStringExtra(NewBucketDialogFragment.KEY_BUCKET_NAME);
             String bucketDescription = data.getStringExtra(NewBucketDialogFragment.KEY_BUCKET_DESCRIPTION);
             if (!TextUtils.isEmpty(bucketName)) {
-                AsyncTaskCompat.executeParallel(new newBucketTask(bucketName, bucketDescription));
+                new newBucketTask(bucketName, bucketDescription).execute();
             }
         }
     }
